d5063ecedaa1c973c01c4e6c64c5e3b1de35a4b0
Nurul Huda <ibnujakaria@outlook.com>
2020-03-15 10:39:23 +0700

    manipulasi file part 2

---
+++ 13-manipulasi-file-part-2/index.php
@@ -0,0 +1,16 @@
+<?php
+
+// $namaFile = 'nama-bulan.txt';
+// $file = fopen($namaFile, 'a');
+
+// $konten = "Juni\nJuli\nAgustus\nSeptember\nOktober";
+
+// fwrite($file, $konten);
+
+// fclose($file);
+
+// copy('nama-bulan.txt', 'nama-bulan-indo.txt');
+
+// rename('nama-bulan.txt', 'nama-bulan-old.txt');
+
+// unlink('nama-bulan-old.txt');
